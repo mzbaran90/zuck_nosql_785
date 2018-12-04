18b4752510bfbdef889974b856c1a5d6f80e43e4
Mason Edmison <mzbaran@uwm.edu>
2018-12-04 15:33:23 -0600

    formatting

--- JsonCreate.py
+++ JsonCreate.py
@@ -1,6 +1,5 @@
 import json
-from xml.etree import ElementTree as ET
-import Record
+
 
 '''this class takes a record and returns a formatted json document
 A record consists of node element objects and arrays. node elements will be accessed by calling <node>.text'''
@@ -19,7 +18,8 @@ class JSON(object):
         unOfficJSON = {'record_id': self.record_id, 'title': self.title, 'speakers': self.record.speakers,
                        'category': self.category, 'date': self.date, 'url': self.url,
                        'statements': self.record.statements}
+
         officJSON = json.dumps(unOfficJSON, indent= 4)
 
-        print(officJSON)
+        
         return officJSON
